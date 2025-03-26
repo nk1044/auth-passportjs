e9967cd44446bc8410fd6b80dd5ff487ca076ed6
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-26 15:52:34 +0530

    update API endpoints to use backend URL from environment variables

--- frontend/src/Server/server.js
+++ frontend/src/Server/server.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const backendUrl = String(import.meta.env.VITE_BACKEND_URL);
+
 const login = async (email, password) => {
-  const response = await axios.post('http://localhost:9000/api/auth/login', { email, password });
+  const response = await axios.post(`${backendUrl}/api/auth/login`, { email, password });
   console.log(response.data);
   return response.data;
 }
 const register = async (email, password) => {
-  const response = await axios.post('http://localhost:9000/api/auth/register', { email, password });
+  const response = await axios.post(`${backendUrl}/api/auth/register`, { email, password });
   console.log(response.data);
   return response.data;
 }
